27642663af7176300bdd8cf53a844ed729583d78
Nitin Upreti <nitin.upreti@tadigital.com>
2024-02-16 15:53:58 +0530

    Added changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,7 +14,19 @@ import {
 } from './aem.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
+const AUDIENCES = {
+  mobile: () => window.innerWidth < 600,
+  desktop: () => window.innerWidth >= 600,
+  // define your custom audiences here as needed
+};
 
+window.hlx.plugins.add('experimentation', {
+  condition: () => getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length,
+  options: { audiences: AUDIENCES },
+  url: '/plugins/experimentation/src/index.js',
+});
 // Add you templates below
 // window.hlx.templates.add('/templates/my-template');
 
