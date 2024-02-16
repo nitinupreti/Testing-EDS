2922cd8143a7e4c67628480eb738c8ade3883c10
Nitin Upreti <nitin.upreti@tadigital.com>
2024-02-16 18:45:19 +0530

    Added changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,6 +14,36 @@ import {
   getMetadata,
 } from './aem.js';
 
+const LCP_BLOCKS = []; // add your LCP blocks to the list
+const AUDIENCES = {
+  mobile: () => window.innerWidth < 600,
+  desktop: () => window.innerWidth >= 600,
+  // define your custom audiences here as needed
+};
+/**
+ * Gets all the metadata elements that are in the given scope.
+ * @param {String} scope The scope/prefix for the metadata
+ * @returns an array of HTMLElement nodes that match the given scope
+ */
+export function getAllMetadata(scope) {
+  return [...document.head.querySelectorAll(`meta[property^="${scope}:"],meta[name^="${scope}-"]`)]
+    .reduce((res, meta) => {
+      const id = toClassName(meta.name
+        ? meta.name.substring(scope.length + 1)
+        : meta.getAttribute('property').split(':')[1]);
+      res[id] = meta.getAttribute('content');
+      return res;
+    }, {});
+}
+
+window.hlx.plugins.add('experimentation', {
+  condition: () => getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length,
+  options: { audiences: AUDIENCES },
+  url: '/plugins/experimentation/src/index.js',
+});
+
 const pluginContext = {
   getAllMetadata,
   getMetadata,
@@ -24,20 +54,6 @@ const pluginContext = {
   toClassName,
 };
 
-const LCP_BLOCKS = []; // add your LCP blocks to the list
-const AUDIENCES = {
-  mobile: () => window.innerWidth < 600,
-  desktop: () => window.innerWidth >= 600,
-  // define your custom audiences here as needed
-};
-
-window.hlx.plugins.add('experimentation', {
-  condition: () => getMetadata('experiment')
-    || Object.keys(getAllMetadata('campaign')).length
-    || Object.keys(getAllMetadata('audience')).length,
-  options: { audiences: AUDIENCES },
-  url: '/plugins/experimentation/src/index.js',
-});
 // Add you templates below
 // window.hlx.templates.add('/templates/my-template');
 
